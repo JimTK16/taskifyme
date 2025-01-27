a851a8c9fd96a777797835406caf5678d2c15a87
JimTK16 <longduy88@gmail.com>
2025-01-27 13:06:35 +1100

    add protected route and new api files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import SideBar from './components/SideBar'
 import { Grid2 } from '@mui/material'
-import { BrowserRouter, Route, Outlet, Routes } from 'react-router'
+import { BrowserRouter, Route, Outlet, Routes, Navigate } from 'react-router'
 import Today from './components/Today'
 import Upcoming from './components/Upcoming'
 import Inbox from './components/Inbox'
-
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import ProtectedRoute from './components/auth/ProtectedRoute'
 const Layout = () => {
   return (
     <Grid2 container>
@@ -30,11 +32,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Today />} />
-          <Route path="upcoming" element={<Upcoming />} />
-          <Route path="today" element={<Today />} />
-          <Route path="inbox" element={<Inbox />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="today" replace />} />
+            <Route path="upcoming" element={<Upcoming />} />
+            <Route path="today" element={<Today />} />
+            <Route path="inbox" element={<Inbox />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
